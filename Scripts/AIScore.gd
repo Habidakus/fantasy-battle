f7fc66ef78d322c7817fc45b032e994837b26527
Jason Abbott <woebane@gmail.com>
2025-10-15 16:02:10 -0700

    Take into account how many dice we have over the enemy when making calculations

--- Scripts/AIScore.gd
+++ Scripts/AIScore.gd
@@ -1,10 +1,11 @@
 class_name AIScore extends MMCScore
 
 var _get_close : float = 0
+var _die_supremacy : float = 0
 var _jcounter : JCounter = JCounter.Create("AIScore")
 
 func _to_string() -> String:
-	return "score=%s" % [_get_close]
+	return "score=(%s, %s)" % [_die_supremacy, _get_close]
 
 static func Create(game_state : GameState) -> AIScore:
 	var aiArmy : Army = game_state.GetInvokingArmy()
@@ -13,9 +14,13 @@ static func Create(game_state : GameState) -> AIScore:
 	var ret_val : AIScore = AIScore.new()
 	for squad : Squad in aiArmy._squads:
 		var base_score : float = squad.GetBaseScore()
+		ret_val._die_supremacy += base_score
 		for enemy : Squad in otherArmy._squads:
 			var enemy_base_score : float = enemy.GetBaseScore()
 			ret_val._get_close += squad.GetClosenessScore(base_score, enemy_base_score, squad.position.distance_squared_to(enemy.position))
+	for enemy : Squad in otherArmy._squads:
+		var enemy_base_score : float = enemy.GetBaseScore()
+		ret_val._die_supremacy -= enemy_base_score
 	return ret_val
 
 ## Return the inverse of the current score. If the score is being kept in a simple numeric value, this
@@ -24,6 +29,7 @@ static func Create(game_state : GameState) -> AIScore:
 func reversed() -> MMCScore:
 	var ret_val : AIScore = AIScore.new()
 	ret_val._get_close = 0 - _get_close
+	ret_val._die_supremacy = 0 - _die_supremacy
 	return ret_val
 
 ## Returns true only if the current score is better for the Computer Player (the one we're doing all this
@@ -31,6 +37,8 @@ func reversed() -> MMCScore:
 func is_better_than(other : MMCScore) -> bool:
 	if other is AIScore:
 		var other_ai_score : AIScore = other as AIScore
+		if _die_supremacy != other_ai_score._die_supremacy:
+			return _die_supremacy > other_ai_score._die_supremacy
 		return _get_close > other_ai_score._get_close
 	assert(false)
 	return false
