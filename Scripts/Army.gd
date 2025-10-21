19cda35a890df41bb1378c9206b5a70fb09f7e91
Jason Abbott <woebane@gmail.com>
2025-10-20 19:08:33 -0700

    More whitespace nonsense between the two editor configurations

--- Scripts/Army.gd
+++ Scripts/Army.gd
@@ -6,43 +6,43 @@ var _controller : ArmyController
 var _jcounter : JCounter = JCounter.Create("Army")
 
 func Clone() -> Army:
-    var ret_val : Army = Army.new()
-    ret_val.SetColor(_color)
-    ret_val.SetController(_controller)
-    for s : Squad in _squads:
-        ret_val.Add(s.Clone())
-    return ret_val
+	var ret_val : Army = Army.new()
+	ret_val.SetColor(_color)
+	ret_val.SetController(_controller)
+	for s : Squad in _squads:
+		ret_val.Add(s.Clone())
+	return ret_val
 
 func GetPrimaryColor() -> Color:
-    return _color
+	return _color
 
 func GetSecondaryColor() -> Color:
-    var h : float = _color.h + 0.5
-    if h > 1.0:
-        h -= 1.0
-    var s : float = _color.s
-    if s > 0.9:
-        s = 0.5
-    else:
-        s = 1.0
-    var v : float = _color.v
-    if v > 0.9:
-        v = 0.5
-    else:
-        v = 1.0
-    return Color.from_hsv(h, s, v)
+	var h : float = _color.h + 0.5
+	if h > 1.0:
+		h -= 1.0
+	var s : float = _color.s
+	if s > 0.9:
+		s = 0.5
+	else:
+		s = 1.0
+	var v : float = _color.v
+	if v > 0.9:
+		v = 0.5
+	else:
+		v = 1.0
+	return Color.from_hsv(h, s, v)
 
 func Add(squad : Squad) -> void:
-    _squads.append(squad)
+	_squads.append(squad)
 
 func SetColor(color : Color) -> void:
-    _color = color
+	_color = color
 
 func SetController(controller : ArmyController) -> void:
-    _controller = controller
+	_controller = controller
 
 func GetColor() -> Color:
-    return _color
+	return _color
 
 func GetController() -> AIArmyController:
-    return _controller
+	return _controller
--- Scripts/ArmyControllerAction.gd
+++ Scripts/ArmyControllerAction.gd
@@ -10,116 +10,116 @@ var _position : Vector2
 var _jcounter : JCounter = JCounter.Create("ArmyControllerAction")
 
 func _to_string() -> String:
-    if _squad == null:
-        assert(_action == Action.PASS)
-        return "Controller passes"
-    var squad_name : String = "%s's %s" % [_squad.GetArmy().GetController(), _squad]
-    match _action:
-        Action.PASS:
-            return "%s will wait" % [squad_name]
-        Action.MELEE:
-            var target_name : String = str(_target)
-            return "%s will melee %s" % [squad_name, target_name]
-        Action.CHARGE:
-            var target_name : String = str(_target)
-            return "%s will charge %s" % [squad_name, target_name]
-        Action.MOVE:
-            if _target != null:
-                var target_name : String = str(_target)
-                return "%s will move towards %s" % [squad_name, target_name]
-            else:
-                return "%s will move to location" % [squad_name]
-        _:
-            return "%s will perform unknown action: %s" % [squad_name, Action.keys()[_action]]
+	if _squad == null:
+		assert(_action == Action.PASS)
+		return "Controller passes"
+	var squad_name : String = "%s's %s" % [_squad.GetArmy().GetController(), _squad]
+	match _action:
+		Action.PASS:
+			return "%s will wait" % [squad_name]
+		Action.MELEE:
+			var target_name : String = str(_target)
+			return "%s will melee %s" % [squad_name, target_name]
+		Action.CHARGE:
+			var target_name : String = str(_target)
+			return "%s will charge %s" % [squad_name, target_name]
+		Action.MOVE:
+			if _target != null:
+				var target_name : String = str(_target)
+				return "%s will move towards %s" % [squad_name, target_name]
+			else:
+				return "%s will move to location" % [squad_name]
+		_:
+			return "%s will perform unknown action: %s" % [squad_name, Action.keys()[_action]]
 
 func ApplyToBoardState(board_state : BoardState, rnd : RandomNumberGenerator, prnt : bool) -> void:
-    match _action:
-        Action.PASS:
-            _apply_pass(prnt, board_state)
-        Action.MELEE:
-            _apply_melee(prnt, board_state, rnd)
-        Action.CHARGE:
-            _apply_charge(prnt, board_state, rnd)
-        Action.MOVE:
-            _apply_move(prnt, board_state)
-        _:
-            assert(false, "Unknown action type: " + Action.keys()[_action])
+	match _action:
+		Action.PASS:
+			_apply_pass(prnt, board_state)
+		Action.MELEE:
+			_apply_melee(prnt, board_state, rnd)
+		Action.CHARGE:
+			_apply_charge(prnt, board_state, rnd)
+		Action.MOVE:
+			_apply_move(prnt, board_state)
+		_:
+			assert(false, "Unknown action type: " + Action.keys()[_action])
 
 func _apply_pass(prnt: bool, board_state : BoardState) -> void:
-    if _squad != null:
-        if prnt:
-            print("%s passes" % [_squad])
-        board_state.DelaySquad(_squad.id, 2)
-    else:
-        if prnt:
-            print("Player passes")
+	if _squad != null:
+		if prnt:
+			print("%s passes" % [_squad])
+		board_state.DelaySquad(_squad.id, 2)
+	else:
+		if prnt:
+			print("Player passes")
 
 func _apply_melee(prnt: bool, board_state : BoardState, rnd : RandomNumberGenerator) -> void:
-    board_state.InflictDamage(prnt, _squad.id, _target.id, Squad.DamageType.MELEE, rnd)
-    var both_alive : bool = true
-    if board_state.RemoveSquadIfDead(_target.id):
-        both_alive = false
-    if board_state.RemoveSquadIfDead(_squad.id):
-        both_alive = false
-    else:
-        board_state.DelaySquad(_squad.id, _squad.GetMeleeTime())
-    if both_alive:
-        board_state.MarkInCombat(_squad.id, _target.id)
+	board_state.InflictDamage(prnt, _squad.id, _target.id, Squad.DamageType.MELEE, rnd)
+	var both_alive : bool = true
+	if board_state.RemoveSquadIfDead(_target.id):
+		both_alive = false
+	if board_state.RemoveSquadIfDead(_squad.id):
+		both_alive = false
+	else:
+		board_state.DelaySquad(_squad.id, _squad.GetMeleeTime())
+	if both_alive:
+		board_state.MarkInCombat(_squad.id, _target.id)
 
 func _apply_charge(prnt: bool, board_state : BoardState, rnd : RandomNumberGenerator) -> void:
-    board_state.MoveTowardsTarget(_squad.id, _target.id)
-    board_state.InflictDamage(prnt, _squad.id, _target.id, Squad.DamageType.CHARGE, rnd)
-    var both_alive : bool = true
-    if board_state.RemoveSquadIfDead(_target.id):
-        both_alive = false
-    if board_state.RemoveSquadIfDead(_squad.id):
-        both_alive = false
-    else:
-        board_state.DelaySquad(_squad.id, _squad.GetChargeTime())
-    if both_alive:
-        board_state.MarkInCombat(_squad.id, _target.id)
+	board_state.MoveTowardsTarget(_squad.id, _target.id)
+	board_state.InflictDamage(prnt, _squad.id, _target.id, Squad.DamageType.CHARGE, rnd)
+	var both_alive : bool = true
+	if board_state.RemoveSquadIfDead(_target.id):
+		both_alive = false
+	if board_state.RemoveSquadIfDead(_squad.id):
+		both_alive = false
+	else:
+		board_state.DelaySquad(_squad.id, _squad.GetChargeTime())
+	if both_alive:
+		board_state.MarkInCombat(_squad.id, _target.id)
 
 func _apply_move(prnt: bool, board_state : BoardState) -> void:
-    if prnt:
-        print("%s advances" % [_squad])
-    if _target != null:
-        board_state.MoveTowardsTarget(_squad.id, _target.id)
-    else:
-        board_state.MoveTowardsLocation(_squad.id, _position)
-    board_state.DelaySquad(_squad.id, _squad.GetMoveTime())
+	if prnt:
+		print("%s advances" % [_squad])
+	if _target != null:
+		board_state.MoveTowardsTarget(_squad.id, _target.id)
+	else:
+		board_state.MoveTowardsLocation(_squad.id, _position)
+	board_state.DelaySquad(_squad.id, _squad.GetMoveTime())
 
 static func _create(squad : Squad, action : Action) -> ArmyControllerAction:
-    var ret_val :ArmyControllerAction = ArmyControllerAction.new()
-    ret_val._action = action
-    ret_val._squad = squad
-    return ret_val
+	var ret_val :ArmyControllerAction = ArmyControllerAction.new()
+	ret_val._action = action
+	ret_val._squad = squad
+	return ret_val
 
 static func CreateSidePass(_controller : ArmyController) -> ArmyControllerAction:
-    return ArmyControllerAction.new()
+	return ArmyControllerAction.new()
 
 static func CreateMelee(squad : Squad, target : Squad) -> ArmyControllerAction:
-    assert(squad.GetArmy() != target.GetArmy())
-    var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.MELEE)
-    ret_val._target = target
-    return ret_val
+	assert(squad.GetArmy() != target.GetArmy())
+	var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.MELEE)
+	ret_val._target = target
+	return ret_val
 
 static func CreatePass(squad : Squad) -> ArmyControllerAction:
-    return ArmyControllerAction._create(squad, Action.PASS)
+	return ArmyControllerAction._create(squad, Action.PASS)
 
 static func CreateMoveAt(squad : Squad, target : Squad) -> ArmyControllerAction:
-    assert(squad.GetArmy() != target.GetArmy())
-    var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.MOVE)
-    ret_val._target = target
-    return ret_val
+	assert(squad.GetArmy() != target.GetArmy())
+	var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.MOVE)
+	ret_val._target = target
+	return ret_val
 
 static func CreateMoveTowards(squad : Squad, loc : Vector2) -> ArmyControllerAction:
-    var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.MOVE)
-    ret_val._position = loc
-    #ret_val._rotation = squad.position.angle_to(loc)
-    return ret_val
+	var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.MOVE)
+	ret_val._position = loc
+	#ret_val._rotation = squad.position.angle_to(loc)
+	return ret_val
 
 static func CreateCharge(squad : Squad, target : Squad) -> ArmyControllerAction:
-    assert(squad.GetArmy() != target.GetArmy())
-    var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.CHARGE)
-    ret_val._target = target
-    return ret_val
+	assert(squad.GetArmy() != target.GetArmy())
+	var ret_val : ArmyControllerAction = ArmyControllerAction._create(squad, Action.CHARGE)
+	ret_val._target = target
+	return ret_val
