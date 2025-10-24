61d908cf09674a07d0cdb41de1dccd26d1b80a6c
Jason Abbott <woebane@gmail.com>
2025-10-24 12:33:49 -0700

    more whitespace melarchy

--- Scripts/AIArmyController.gd
+++ Scripts/AIArmyController.gd
@@ -7,16 +7,16 @@ var _turn_engine : TurnEngine
 var _negamax_engine : MinMaxCalculator = MinMaxCalculator.new()
 
 func _to_string() -> String:
-	return _name
+    return _name
 
 func RequestOrders(squad : Squad) -> void:
-	var game_state : GameState = _turn_engine.GenerateGameState(squad)
-	var action : ArmyControllerAction = _negamax_engine.get_best_action(game_state, 8)
-	if action != null:
-		_turn_engine.SubmitAction(action)
+    var game_state : GameState = _turn_engine.GenerateGameState(squad)
+    var action : ArmyControllerAction = _negamax_engine.get_best_action(game_state, 8)
+    if action != null:
+        _turn_engine.SubmitAction(action)
 
 func RegisterTurnEngine(turn_engine : TurnEngine) -> void:
-	_turn_engine = turn_engine
-	s_NEXT_ID += 1
-	assert(s_NEXT_ID < 5)
-	_name = "AI#%d" % [s_NEXT_ID]
+    _turn_engine = turn_engine
+    s_NEXT_ID += 1
+    assert(s_NEXT_ID < 5)
+    _name = "AI#%d" % [s_NEXT_ID]
--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -8,76 +8,76 @@ var _rnd : RandomNumberGenerator = RandomNumberGenerator.new() # We should imple
 var _jcounter : JCounter = JCounter.Create("GameState")
 
 static func Create(squad : Squad, armies : Array[Army], terrain_data : TerrainData, in_combat : Array[int]) -> GameState:
-	var ret_val : GameState = GameState.new()
-	ret_val._squad = squad
-	ret_val._board_state.Config(armies, terrain_data, in_combat)
-	ret_val._invoking_controller = squad.GetArmy().GetController()
-	ret_val._current_controller = ret_val._invoking_controller
-	assert(ret_val._invoking_controller is AIArmyController)
-	return ret_val
+    var ret_val : GameState = GameState.new()
+    ret_val._squad = squad
+    ret_val._board_state.Config(armies, terrain_data, in_combat)
+    ret_val._invoking_controller = squad.GetArmy().GetController()
+    ret_val._current_controller = ret_val._invoking_controller
+    assert(ret_val._invoking_controller is AIArmyController)
+    return ret_val
 
 func GetInvokingArmy() -> Army:
-	return _board_state.GetControllersArmy(_invoking_controller)
+    return _board_state.GetControllersArmy(_invoking_controller)
 
 func GetNonInvokingArmy() -> Army:
-	return _board_state.GetControllersArmy(_get_opposing_controller(_invoking_controller))
+    return _board_state.GetControllersArmy(_get_opposing_controller(_invoking_controller))
 
 func get_score() -> AIScore:
-	return AIScore.Create(self)
+    return AIScore.Create(self)
 
 func get_sorted_moves(_for_requesting_player : bool) -> Array[MMCAction]:
-	if _squad == null:
-		return []
-	if _current_controller == _squad.GetArmy().GetController():
-		return _squad.GetSortedMoves(self)
-	else:
-		var ret_val : Array[MMCAction]
-		ret_val.append(ArmyControllerAction.CreateSidePass(_current_controller))
-		return ret_val
+    if _squad == null:
+        return []
+    if _current_controller == _squad.GetArmy().GetController():
+        return _squad.GetSortedMoves(self)
+    else:
+        var ret_val : Array[MMCAction]
+        ret_val.append(ArmyControllerAction.CreateSidePass(_current_controller))
+        return ret_val
 
 func CheckForCollisions(travelling_squad : Squad, destination : Vector2) -> Array:
-	return _board_state.CheckForCollisions(travelling_squad, destination)
+    return _board_state.CheckForCollisions(travelling_squad, destination)
 
 func AssignNextSquadToGo() -> void:
-	_squad = _board_state.GetNextSquadToGo()
+    _squad = _board_state.GetNextSquadToGo()
 
 func GetSquadById(id : int) -> Squad:
-	return _board_state.GetSquadById(id)
+    return _board_state.GetSquadById(id)
 
 func RemoveSquadIfDead(id : int) -> bool:
-	return _board_state.RemoveSquadIfDead(id)
+    return _board_state.RemoveSquadIfDead(id)
 
 func CreateClone() -> GameState:
-	var ret_val : GameState = GameState.new()
-	ret_val._board_state = _board_state.Clone()
-	ret_val._invoking_controller = _invoking_controller
-	ret_val._squad = null
-	ret_val._current_controller = _get_opposing_controller(_current_controller)
-	return ret_val
+    var ret_val : GameState = GameState.new()
+    ret_val._board_state = _board_state.Clone()
+    ret_val._invoking_controller = _invoking_controller
+    ret_val._squad = null
+    ret_val._current_controller = _get_opposing_controller(_current_controller)
+    return ret_val
 
 func _get_opposing_controller(controller : AIArmyController) -> ArmyController:
-	return _board_state.GetOpposingController(controller)
+    return _board_state.GetOpposingController(controller)
   
 func apply_action(action : MMCAction) -> MMCGameState:
-	var ret_val : GameState = CreateClone()
-	var aca : ArmyControllerAction = action as ArmyControllerAction
-	aca.ApplyToBoardState(ret_val._board_state, ret_val._rnd, false)
-	ret_val.AssignNextSquadToGo()
-	return ret_val
+    var ret_val : GameState = CreateClone()
+    var aca : ArmyControllerAction = action as ArmyControllerAction
+    aca.ApplyToBoardState(ret_val._board_state, ret_val._rnd, false)
+    ret_val.AssignNextSquadToGo()
+    return ret_val
 
 func IsInCombat(squad : Squad) -> bool:
-	return _board_state.IsInCombat(squad.id)
+    return _board_state.IsInCombat(squad.id)
 
 func LockedInCombat(left : int, right : int) -> bool:
-	return _board_state.LockedInCombat(left, right)
+    return _board_state.LockedInCombat(left, right)
 
 func GetAllEnemy(army : Army) -> Array[Squad]:
-	return _board_state.GetAllEnemy(army)
+    return _board_state.GetAllEnemy(army)
 
 func GetAllChargableEnemy(squad : Squad) -> Array[Squad]:
-	assert(!squad.IsDead())
-	var ret_val : Array[Squad]
-	for e : Squad in GetAllEnemy(squad.GetArmy()):
-		if squad.CanCharge(e):
-			ret_val.append(e)
-	return ret_val
+    assert(!squad.IsDead())
+    var ret_val : Array[Squad]
+    for e : Squad in GetAllEnemy(squad.GetArmy()):
+        if squad.CanCharge(e):
+            ret_val.append(e)
+    return ret_val
